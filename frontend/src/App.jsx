1f0185f746bf84a946ca0348769b78ee71579239
aBugInTheSystem <ekom338.com@gmail.com>
2023-11-10 21:32:52 +0300

    Navbar and react-router added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Banner from "./components/Banner";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Banner />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
