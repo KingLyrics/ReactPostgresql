c81b76d98b146526bb0e2e69019d82d8d76b93f1
aBugInTheSystem <ekom338.com@gmail.com>
2023-11-11 00:11:19 +0300

    Hidden apiUrl and processes on the frontend

--- frontend/src/pages/BlogPost.jsx
+++ frontend/src/pages/BlogPost.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const BlogPost = () => {
   const { id } = useParams();
@@ -8,7 +9,7 @@ const BlogPost = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4321/api/posts/${id}`)
+      .get(`${apiUrl}/api/posts/${id}`)
       .then((response) => {
         setBlog(response.data);
       })
@@ -19,11 +20,12 @@ const BlogPost = () => {
   }, [id]);
 
   return (
-    <div>
+    <div className="container mt-5">
       {blog ? (
         <div>
-          <h1>{blog.title}</h1>
-          <p>{blog.content}</p>
+          <h1 className="fs-1">Title: {blog.title}</h1>
+          <p className="fs-5">Authored by: {blog.author}</p>
+          <p className="fs-3 text-break fw-light">{blog.content}</p>
         </div>
       ) : (
         <p>No blog post found.</p>
